63f8bb79f9a5dae01600f37022df1d14394cf0cd
Nevo0 <Piecyk.rafal@gmail.com>
2019-04-08 20:25:16 +0200

    sync get data

--- projects/app.js
+++ projects/app.js
@@ -1,6 +1,6 @@
 // variables
 
-const cartBtn = document.querySelectoe(".cart-btn");
+const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
@@ -9,3 +9,51 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
+
+//cart
+let cart = [];
+
+//getting products
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch("products.json");
+            let data = await result.json();
+            let products = data.items
+            products = products.map(item => {
+                const {
+                    title,
+                    price
+                } = item.fields;
+                const {
+                    id
+                } = item.sys
+                const image = item.fields.image.fields.file.url
+                return {
+                    title,
+                    price,
+                    id,
+                    image
+                }
+            })
+            return products
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+//display priducts
+class UI {}
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI()
+    const products = new Products()
+
+    //get all product
+
+    products.getProducts().then(data => console.log(data))
+})
